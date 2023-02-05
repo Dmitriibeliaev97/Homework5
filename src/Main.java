01d550cd01bbbe57cf3b97e6aee0346c9e075818
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-02-05 12:11:23 +0300

    Создал начало под каждое здание, сохранил пример из урока

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 6;
+        switch (age) {
+            case 18:
+                System.out.println("С окончанием школы!");
+                break;
+            case 21:
+                System.out.println("Теперь можно пить алкоголь");
+                break;
+            case 7:
+                System.out.println("Иди в школу");
+                break;
+            default:
+                System.out.println("Пока сказать нечего");
+        }
+
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
     }
 }
